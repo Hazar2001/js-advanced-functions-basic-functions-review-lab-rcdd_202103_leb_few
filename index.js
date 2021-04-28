022294e1b7d755c79fa12459fa3bae03e36d4d64
Hazar Saad ElDine <saadeldinehazar@gmail.com>
2021-04-28 10:12:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ return "This Saturday, I want to "+ string +"!";
 saturdayFun();
 saturdayFun("bathe my dog");
 
-function mondayWork(string="go to the office."){
-return "This Monday, I will "+ string;
+function mondayWork(string="go to the office"){
+return "This Monday, I will "+ string + ".";
 }
 mondayWork();
-mondayWork("work from home.");
+mondayWork("work from home");
 
 function wrapAdjective(string="*"){
   return function(s="a hard worker"){
@@ -28,17 +28,20 @@ const Calculator={
 
 function actionApplyer(s="0",array){
 array=[multiply(s),add(s),modulo(s)];
- function result() {
+
 
    function multiply(s){
-     return s*2;
+     s1=s*2;
+     return s1;
    }
    function add(s){
-     return s+1000;
+     s2= s1+1000;
+     return s2;
    }
    function modulo(s){
-     return s%7;;
+     s3= s2%7;
+     return s3;
    }
-    return s;
- }
+   return s3;
+  
  }
